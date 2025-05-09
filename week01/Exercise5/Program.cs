132628bca02d390fe8b7be71ea25c13342970de9
Travis Nelson <tjnelson@familysearch.org>
2025-05-08 18:16:39 -0600

    Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+
+        String userName = GetUserName();
+        int userAge = GetUserAge();
+
+        int squareAge = CalculateSquareOfAge(userAge);
+        DisplayUserInfo(userName, squareAge);
+    }
+
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Week 1 Exercise 5 program!");
+    }
+
+    static String GetUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int GetUserAge()
+    {
+        Console.Write("Please enter your age: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int CalculateSquareOfAge(int age)
+    {
+        return age * age;
+    }
+
+    static void DisplayUserInfo(String name, int squareAge)
+    {
+        Console.WriteLine($"Hello {name}, the square of your age is {squareAge}.");
     }
 }
