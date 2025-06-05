cbcf9c764bb454eee737687884a2ea9e6b10a014
Travis Nelson <tjnelson@familysearch.org>
2025-06-04 19:18:12 -0600

    Add WritingAssignment class and test its functionality in Program

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -13,5 +13,10 @@ class Program
         MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
         Console.WriteLine(mathAssignment.GetSummary());
         Console.WriteLine(mathAssignment.GetHomeworkList());
+        
+        // Test writing assignment
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
